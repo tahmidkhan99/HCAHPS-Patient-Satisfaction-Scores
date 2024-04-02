95d4e5a45f23688ac5b99772343cc351dc61957a
Tahmid Khan <125160830+tahmidkhan99@users.noreply.github.com>
2024-04-02 18:25:21 -0400

    HCAHPS data + SQL code for cleaning

---
+++ hcahps.sql
@@ -0,0 +1,25 @@
+create table "postgres"."hospital_data".Tableau_File as
+	
+with hospital_beds_prep as
+(
+select lpad(cast(provider_ccn as text),6,'0') as provider_ccn
+	,hospital_name
+	,to_date(fiscal_year_begin_date,'MM/DD/YYYY') as fiscal_year_begin_date
+	,to_date(fiscal_year_end_date,'MM/DD/YYYY') as fiscal_year_end_date
+	,number_of_beds
+	,row_number() over (partition by provider_ccn order by to_date(fiscal_year_end_date,'MM/DD/YYYY') desc) as nth_row
+from "postgres"."hospital_data".hospital_beds
+)
+
+select lpad(cast(facility_id as text),6,'0') as provider_ccn
+	,to_date(start_date,'MM/DD/YY') as start_date_converted
+	,to_date(end_date,'MM/DD/YY') as end_date_converted
+	,hcahps.*
+	,beds.number_of_beds
+	,beds.fiscal_year_begin_date as beds_start_report_period
+	,beds.fiscal_year_end_date as bed_end_report_period
+from "postgres"."hospital_data".hcahps_data as hcahps
+left join hospital_beds_prep as beds
+on lpad(cast(facility_id as text),6,'0') = beds.provider_ccn
+and beds.nth_row = 1
+
